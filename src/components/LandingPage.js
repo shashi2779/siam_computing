fee40813cce2ceec12970699280d7f3dbd3aaf12
shashi2779 <yadavshashikant2779@gmail.com>
2023-06-30 02:04:13 +0530

    tgrwtg

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -13,6 +13,10 @@ function LandingPage() {
                 <div className='landingpage'>
                     <div className='landing-content'>
                         <h1>Startups need to scale fast. We get it.</h1>
+                        <div className='landing-imggg'>
+                            <img src={slazzerr} />
+                        </div>
+
                         <p>Our skin in the game philosophy enables multiple iterations, lower cash burn and a faster road to product-market fitment.</p>
                         <a>Get in touch with an expert</a>
                     </div>
@@ -21,7 +25,7 @@ function LandingPage() {
                     </div>
                 </div>
                 <div className='landing-company'>
-                    <div className='company-img' ><img src={nvya} /></div>
+                    <div className='company-img company-img-1' ><img src={nvya} /></div>
                     <div className='company-img' ><img src={xcode} /></div>
                     <div className='company-img' ><img src={nvya} /></div>
                     <div className='company-img' ><img src={xcode} /></div>
