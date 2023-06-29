7490b7e0f41d80d839acecf8193b6d96f51974f1
shashi2779 <yadavshashikant2779@gmail.com>
2023-06-29 20:36:33 +0530

    tytetyt

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import UsSection from './components/UsSection';
+import LandingPage from './components/LandingPage';
 
 function App() {
   return (
     <>
       <Header />
+      <LandingPage/>
       <UsSection />
       <Footer />
     </>
---
+++ src/components/LandingPage.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import './CSS/LandingPage.css'
+import slazzerr from '../img/slazzermain.png'
+import nvya from '../img/Navya.png'
+import xcode from '../img/Xcode-logo.png'
+
+
+
+function LandingPage() {
+    return (
+        <>
+            <div className='landing-all'>
+                <div className='landingpage'>
+                    <div className='landing-content'>
+                        <h1>Startups need to scale fast. We get it.</h1>
+                        <p>Our skin in the game philosophy enables multiple iterations, lower cash burn and a faster road to product-market fitment.</p>
+                        <a>Get in touch with an expert</a>
+                    </div>
+                    <div className='landing-img'>
+                        <img src={slazzerr} />
+                    </div>
+                </div>
+                <div className='landing-company'>
+                    <div className='company-img' ><img src={nvya} /></div>
+                    <div className='company-img' ><img src={xcode} /></div>
+                    <div className='company-img' ><img src={nvya} /></div>
+                    <div className='company-img' ><img src={xcode} /></div>
+                    <div className='company-img' ><img src={nvya} /></div>
+                    <div className='company-img' ><img src={xcode} /></div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default LandingPage
